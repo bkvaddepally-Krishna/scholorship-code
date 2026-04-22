5180eeca3e5923b34b99af14902c2dd89892d0eb
bkvaddepally-Krishna <bkvaddepally@gmail.com>
2026-04-22 20:13:33 +0530

    Delete admin directory

--- admin/api/ui_logger.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../../config/db.php';
-if(isset($_POST['action'])){
-    $action = mysqli_real_escape_string($conn, $_POST['action']);
-    $conn->query("INSERT INTO logs (action) VALUES ('$action')");
-}
-?>
--- admin/notifications/notify.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../../config/db.php';
-
-$conn->query("INSERT INTO logs(action) VALUES('Results Published & Notification Sent')");
-echo "Notification Logged";
-?>
